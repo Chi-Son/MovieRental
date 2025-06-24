688c9c7104eb14b63d3e16b1ba45c48e3ed9b5e1
Chí Sơn <caosychison3011@gmail.com>
2025-06-25 01:50:19 +0700

    MovieManager

--- MovieRental/Movies.cs
+++ MovieRental/Movies.cs
@@ -10,20 +10,29 @@ class Movie
     public int MovieId { get; set; }
     public string Title { get; set; }
     public double RentPrice { get; set; }
-    List<Movie> Movies { get; set; } = new List<Movie>();
-
     public void Display()
     {
-        Console.WriteLine("--Movie--");
-        Console.WriteLine("Title: " + Title);
-        Console.WriteLine("Rent Price: " + RentPrice);
+        Console.WriteLine("  Title: " + Title);
+        Console.WriteLine("  Rent Price: " + RentPrice);
+    }
+
+}
+class MovieManager
+{
+    public List<Movie> Movies { get; set; } = new List<Movie>();
+    public MovieManager()
+    {
+        Movies.Add(new Movie { MovieId = 1, Title = "Friends", RentPrice = 3.9 });
+        Movies.Add(new Movie { MovieId = 2, Title = "How i meet your mother", RentPrice = 2.88 });
+        Movies.Add(new Movie { MovieId = 3, Title = "The Fall of the House of Usher", RentPrice = 4.34 });
+        Movies.Add(new Movie { MovieId = 4, Title = "Zodiac", RentPrice = 2.16 });
     }
     public void AllMovies()
     {
         int nums = 1;
-        foreach(var  movie in Movies)
+        foreach (var movie in Movies)
         {
-            Console.WriteLine("---Movie No."+nums+"---");
+            Console.WriteLine("---Movie No." + nums + "---");
             movie.Display();
             nums++;
         }
